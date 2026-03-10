5cb5b426edd5189a8704908b570e217ef143fd14
Kaela <kramey1@emich.edu>
2026-03-10 15:32:08 -0400

    nav menu finaler update

--- contenttracker.js
+++ contenttracker.js
@@ -1,14 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.getElementById('menu-toggle');
-    const navList = document.getElementById('nav-links');
 
-    if (menuToggle && navList) {
-        menuToggle.addEventListener('click', () => {
-            navList.classList.toggle('show');
-        });
-    } else {
-        console.error("Menu toggle or nav list element not found.");
-    }
-});
 
 
