81f19c2501f41997baf742e2e468c21641d75382
Kaela <kramey1@emich.edu>
2026-03-10 22:36:15 -0400

    part of link tracker

--- contenttracker.js
+++ contenttracker.js
@@ -1,3 +1,16 @@
+function clickCounter() {
+    const x = document.getElementById("result");
+    if (typeof (Storage) !== "undefined") {
+        if (sessionStorage.clickcount) {
+            sessionStorage.clickcount = Number(sessionStorage.clickcount) + 1;
+        } else {
+            sessionStorage.clickcount = 1;
+        }
+        x.innerHTML = "You have viewed " + sessionStorage.clickcount + " docuemtns out of 16!";
+    } else {
+        x.innerHTML = "Sorry, no Web storage support!";
+    }
+}
 
-
+window.onload = <p id="result"></p>
 
